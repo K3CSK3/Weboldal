fed1f494a8a12883ae9a43ea0a3c8fd5bc56908a
K3CSK3 <wiattila8@gmail.com>
2024-02-29 19:49:49 +0100

    2024. 02. 29. - Medium Beginium withium loadingscreenium

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const data = {
-    "eng":{
-       mainTitle:"website",
-       secondTitle:"With help form",
-       dropDownTitleTop:"Games",
-       dropDownTopLeft:"Devblog",
-       dropDownTopRight:"Done",
-       dropDownTitleBottom:"Community",
-       dropDownBottomLeft:"News",
-       dropDownBottomRight:"Sites",/*
-       :"",
-       :"",
+    "eng": {
+        mainTitle: "website",
+        secondTitle: "With help form",
+        dropDownTitleTop: "Games",
+        dropDownTopLeft: "Devblog",
+        dropDownTopRight: "Done",
+        dropDownTitleBottom: "Community",
+        dropDownBottomLeft: "News",
+        dropDownBottomRight: "Sites",
+        changeTheme: "Change Theme",
+        changeLanguage: "Change Language",/*
        :"",
        :"",
        :"",
@@ -24,17 +24,17 @@ const data = {
        :"",
        :""*/
     },
-    "hun":{
-        mainTitle:"weboldal",
-        secondTitle:"Segített",
-        dropDownTitleTop:"Játékok",
-        dropDownTopLeft:"Devblog",
-        dropDownTopRight:"Kész",
-        dropDownTitleBottom:"Közösség",
-        dropDownBottomLeft:"Hírek",
-        dropDownBottomRight:"Oldalak",/*
-        :"",
-        :"",
+    "hun": {
+        mainTitle: "weboldal",
+        secondTitle: "Segített",
+        dropDownTitleTop: "Játékok",
+        dropDownTopLeft: "Devblog",
+        dropDownTopRight: "Kész",
+        dropDownTitleBottom: "Közösség",
+        dropDownBottomLeft: "Hírek",
+        dropDownBottomRight: "Oldalak",
+        changeTheme: "Téma Váltás",
+        changeLanguage: "Nyelv Váltás",/*
         :"",
         :"",
         :"",
@@ -50,8 +50,17 @@ const data = {
         :""*/
     }
 }
+function loadSite() {
+    const loadTime = 2000
+    var loader = document.getElementById("loader")
+    loader.animate([{ opacity: '100%' }, { opacity: '100%' }, { opacity: '0%' }], { duration: loadTime, iterations: 1, fill: 'forwards' })
+    console.log(loader.style.display)
+    setTimeout(() => {
+        loader.style.display = "none"
+    }, loadTime);
+}
 
-function hamburgerClick(){
+function hamburgerClick() {
     let dropdownStyle = document.getElementById("dropdown-menu").style;
     var hamburger = document.querySelector(".hamburger");
     let burgerStyle = document.getElementById("burgerStyle").style;
@@ -59,7 +68,7 @@ function hamburgerClick(){
     hamburger.classList.toggle("is-active");
 
     if (dropdownStyle.animationName === "expand") {
-        dropdownStyle.animationName = "shrink"  
+        dropdownStyle.animationName = "shrink"
         burgerStyle.animationName = "borderToRight"
         imageStyle.animationName = "borderToLeft"
     } else {
@@ -70,10 +79,10 @@ function hamburgerClick(){
     }
 }
 
-function closeDropdown(){
+function closeDropdown() {
     let dropdownStyle = document.getElementById("dropdown-menu").style;
     var hamburger = document.querySelector(".hamburger");
-    if (window.innerWidth > 600){
+    if (window.innerWidth > 600) {
         hamburger.classList.remove("is-active");
 
         dropdownStyle.animationName = "shrink"
@@ -81,24 +90,24 @@ function closeDropdown(){
 }
 
 window.addEventListener("resize", closeDropdown);
-window.addEventListener("load",closeDropdown)
+window.addEventListener("load", closeDropdown)
 
-function title1Expand(){
+function title1Expand() {
     document.getElementById("title1").style.transform = "scale(111%)"
 }
 
-function title2Expand(){
+function title2Expand() {
     document.getElementById("title2").style.transform = "scale(111%)"
 }
 
-function resetTitle(){
+function resetTitle() {
     document.getElementById("title1").style.transform = "scale(105%)"
     document.getElementById("title2").style.transform = "scale(105%)"
 }
 
-function changeTheme(){
+function changeTheme() {
     var theme = document.getElementById("theme")
-    if (!`${theme.src}`.endsWith("dark.png")){
+    if (!`${theme.src}`.endsWith("dark.png")) {
         setToLightTheme()
     }
     else {
@@ -106,9 +115,9 @@ function changeTheme(){
     }
 }
 
-function changeLanguage(){
+function changeLanguage() {
     var language = document.getElementById("language")
-    if (`${language.src}`.endsWith("flag_HU.png")){
+    if (`${language.src}`.endsWith("flag_HU.png")) {
         setToEnglish()
     }
     else {
@@ -121,49 +130,63 @@ window.addEventListener("resize", mediumScreenSize);
 window.addEventListener("load", mediumScreenSize);
 
 
-function mediumScreenSize(){
-    
-    var hamburger = document.querySelector(".hamburger");
-    let navImage = document.getElementById("navbarImage")
-    let dropDownMenu = document.getElementById("dropdown-menu")
-    
-    if (window.innerWidth > 600){
-        navImage.style.animationName = "borderToLeft"
-        hamburger.style.animationName = "borderToRight"
-        navImage.style.display = "none"
-        hamburger.style.display = "none"
-        dropDownMenu.style.display = "none"
+function mediumScreenSize() {
+
+    var hamburger = document.querySelector(".hamburger").style
+    let navImage = document.getElementById("navbarImage").style
+    let dropDownMenu = document.getElementById("dropdown-menu").style
+    let changeLanguageText = document.querySelector(".changeLanguage").style
+    let changeThemeText = document.querySelector(".changeTheme").style
+
+    if (window.innerWidth > 600) {
+        navImage.animationName = "borderToLeft"
+        hamburger.animationName = "borderToRight"
+        navImage.display = "none"
+        hamburger.display = "none"
+        dropDownMenu.display = "none"
+        changeLanguageText.display = "inline"
+        changeThemeText.display = "inline"
     }
-    else{
-        navImage.style.display = "inline"
-        hamburger.style.display = "inline";
+    else {
+        navImage.display = "inline"
+        hamburger.display = "inline"
+        changeLanguageText.display = "none"
+        changeThemeText.display = "none"
     }
 }
 
 function setToDarkTheme() {
+
+    const change = [
+        { "background": "rgba(0,0,0,0.85)", "color": "white" },
+        { "background": "rgba(255,255,255,0.5)", "color": "black" },
+    ]
+    const settings = {
+        duration: 200,
+        iterations: 1,
+        fill: "forwards"
+    }
     var buttons = document.querySelectorAll(".button")
     var navbar = document.querySelector(".navbar")
     var links = document.getElementsByTagName("a")
     var themeBtn = document.getElementById("themeBtn")
     var theme = document.getElementById("theme")
-    var h1Title = document.getElementById("h1title").style
-    var h2Title = document.getElementById("h2title").style
-    var content = document.getElementById("content").style
-    
+    var h1Title = document.getElementById("h1title")
+    var h2Title = document.getElementById("h2title")
+    var content = document.getElementById("content")
+
     navbar.classList.remove("bg-dark")
     navbar.classList.add("bg-light")
 
     theme.src = "images/light.png"
-    
+
     themeBtn.style.backgroundColor = "rgb(52,58,64)"
     theme.style.backgroundColor = "rgb(52,58,64)"
-    h1Title.backgroundColor = "rgba(255,255,255,0.85)"
-    h2Title.backgroundColor = "rgba(255,255,255,0.85)"
-    content.backgroundColor = "rgba(255,255,255,0.5)"
 
-    h1Title.color = "black"
-    h2Title.color = "black"
-    content.color = "black"
+    h1Title.animate(change, settings)
+    h2Title.animate(change, settings)
+    content.animate(change, settings)
+
     h2Title.borderTopColor = "black"
 
     for (const button of buttons) {
@@ -177,30 +200,38 @@ function setToDarkTheme() {
 }
 
 function setToLightTheme() {
+
+    const change = [
+        { "background": "rgba(255,255,255,0.5)", "color": "black" },
+        { "background": "rgba(0,0,0,0.85)", "color": "white" },
+    ]
+    const settings = {
+        duration: 200,
+        iterations: 1,
+        fill: "forwards"
+    }
     var buttons = document.querySelectorAll(".button")
     var navbar = document.querySelector(".navbar")
-    var links = document.getElementsByTagName("a") 
+    var links = document.getElementsByTagName("a")
     var themeBtn = document.getElementById("themeBtn")
     var theme = document.getElementById("theme")
-    var h1Title = document.getElementById("h1title").style
-    var h2Title = document.getElementById("h2title").style
-    var content = document.getElementById("content").style
+    var h1Title = document.getElementById("h1title")
+    var h2Title = document.getElementById("h2title")
+    var content = document.getElementById("content")
 
     navbar.classList.remove("bg-light")
     navbar.classList.add("bg-dark")
 
-    theme.src= "images/dark.png"
+    theme.src = "images/dark.png"
 
 
     themeBtn.style.backgroundColor = "white"
     theme.style.backgroundColor = "white"
-    h1Title.backgroundColor = "rgba(0,0,0,0.85)"
-    h2Title.backgroundColor = "rgba(0,0,0,0.85)"
-    content.backgroundColor = "rgba(0,0,0,0.85)"
 
-    h1Title.color = "white"
-    h2Title.color = "white"
-    content.color = "white"
+    h1Title.animate(change, settings)
+    h2Title.animate(change, settings)
+    content.animate(change, settings)
+
     h2Title.borderTopColor = "white"
 
     for (const button of buttons) {
@@ -219,15 +250,15 @@ function setToHungarian() {
 
     for (const [key, value] of Object.entries(data["hun"])) {
         document.querySelector(`.${key}`).textContent = value
-      }
+    }
 }
 
 function setToEnglish() {
     var language = document.getElementById("language")
-    language.src= "images/flag_EN.png"
+    language.src = "images/flag_EN.png"
 
     for (const [key, value] of Object.entries(data["eng"])) {
         document.querySelector(`.${key}`).textContent = value
-      }
+    }
 }
 
