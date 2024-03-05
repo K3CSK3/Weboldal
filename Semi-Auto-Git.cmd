eef0240d0f17d386d2496b2aadc782ec3e74b1e3
K3CSK3 <114721860+K3CSK3@users.noreply.github.com>
2024-03-05 16:29:29 +0100

    Update Semi-Auto-Git.cmd

--- Semi-Auto-Git.cmd
+++ Semi-Auto-Git.cmd
@@ -1,4 +1,33 @@
+@echo off
 pushd %~0\..
+<<<<<<< HEAD
+:question
+set /p pullorpush="Pull vagy Push? (pull/push) > "
+if /i %pullorpush%==pull (goto :pull) 
+if /i %pullorpush%==push (goto :push) else (goto :question)
+
+:pull
+@echo on
+git pull
+@echo off
+goto :exit
+:push
+set /p comment="Commit neve > "
+
+@echo on
+git add .
+@echo off
+if not %errorlevel%==0 (goto :exit)
+
+@echo on
+git commit -m "%date% - %comment%"
+@echo off
+if not %errorlevel%==0 (goto :exit)
+
+@echo on
+git push
+@echo off
+=======
 
 @echo off
 
@@ -21,8 +50,9 @@ if %mode%==2 (
 timeout 5 /nobreak
 @echo on
 exit /b
+>>>>>>> refs/remotes/Website/main
 
-:error
+:exit
 echo.
 echo.
 timeout 5 /nobreak > nul
